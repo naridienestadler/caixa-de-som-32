c64430128e011593aac8156ec76ef335aefcc699
naridienestadler <105284891+naridienestadler@users.noreply.github.com>
2023-10-10 09:32:27 -0300

    clap

--- main.js
+++ main.js
@@ -7,3 +7,7 @@ function tocaSomPom(){
     document.querySelector('#som_tecla_pom').play();
 }
 document.querySelector('.tecla_clap').onclik = tocaSomClap;
+
+
+const listaDeTlecas = document.querySelectorAll('.tecla');
+istaDeTlecas[1].onclick = TocaSomClap
