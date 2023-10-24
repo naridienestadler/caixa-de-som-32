bfe5473d9289902623186c8898cc936f5b982f4a
naridienestadler <105284891+naridienestadler@users.noreply.github.com>
2023-10-24 09:34:20 -0300

    som

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
 //delaração/criando função tocaSomPom
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
 }
-
 //chamando/invocando a função tocaSomPom pelo clique do botão
-document.querySelector('.tecla_pom').onclik = tocaSomPom;
+document.querySelector('.tecla_pom').onclik = tocaSom;
 
 //referência constante listaDeTeclas, buscando os botões
 const listaDeTlecas = document.querySelectorAll('.tecla');
 
 //chamando a função tocaSomPom pela item "0" da listaDeTeclas
-listaDeTlecas[0].onclick = tocaSomPom;
+listaDeTlecas[0].onclick = tocaSom;
+
 let contador = 0;
-while(contador < 9){
-    listaDeTlecas[contador].onclick = tocaSomPom;
+while(contador < listaDeTlecas.length){
+    const efeito = listaDeTlecas[contador].classList
+    const idAudio = "#som_"+efito;
+    listaDeTlecas[contador].onclick = function({
+        tocaSom(Audio)
+   };
     contador = contador + 1;
     console.log(contador);
 }
