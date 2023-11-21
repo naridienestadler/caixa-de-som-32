c4b706faa3a0b8413ffe54dee3dff2e4fe831f65
naridienestadler <105284891+naridienestadler@users.noreply.github.com>
2023-11-21 09:09:21 -0300

    .

--- main.js
+++ main.js
@@ -6,12 +6,12 @@ function tocaSom(idElementoAudio){
 document.querySelector('.tecla_pom').onclik = tocaSom;
 
 //referência constante listaDeTeclas, buscando os botões
-const listaDeTlecas = document.querySelectorAll('.tecla');
+const listaDeTelecas = document.querySelectorAll('.tecla');
 
 //chamando a função tocaSomPom pela item "0" da listaDeTeclas
-listaDeTlecas[0].onclick = tocaSom;
+listaDeTeclas[0].onclick = tocaSom;
 
-for(let contador = 0;contador<listaDeTlecas.length;contador++){
+for(let contador = 0;contador<listaDeTeclas.length;contador++){
     const tecla = listaDeTeclas [contaor];
     const efeito = tecla.classList[1];
     const idAudio = `#som_${efeito}`;
